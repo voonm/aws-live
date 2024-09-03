1ef299debbf34251afe09a5cde19aab1252fbfc2
voonm <33613056+voonm@users.noreply.github.com>
2024-09-03 20:57:06 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.cqnvzv8ss8bz.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "bait3273"
+customdb = "employee"
+custombucket = "bait3273-employee"
+customregion = "us-east-1"
+

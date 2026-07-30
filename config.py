def19cc58f45716b526751416d5c06d84488c7f6
voonm <33613056+voonm@users.noreply.github.com>
2026-07-30 14:02:36 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-2.cqnvzv8ss8bz.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "bait3273"
+customhost = "db9.cgpul9avgdqf.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "qwer1234"
 customdb = "employee"
-custombucket = "bait3273-employee"
+custombucket = "practical9-employee"
 customregion = "us-east-1"
 
